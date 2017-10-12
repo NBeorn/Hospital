57e588793de19b396710ecf52ebd21a27cd9b2cf
NBeorn <nmbeorn@gmail.com>
2017-10-12 13:33:18 -0400

    doctor class

---
+++ Hospital/Doctor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    class Doctor : Employee
+    {
+        protected string specialtyArea;
+
+        public string SpecialtyArea
+        {
+            get { return this.specialtyArea; }
+        }
+
+        public Doctor(string employeeName, int employeeNumber, string specialtyArea)
+        {
+            this.employeeName = employeeName;
+            this.employeeNumber = employeeNumber;
+            this.specialtyArea = specialtyArea;
+        }
+    }
+}
