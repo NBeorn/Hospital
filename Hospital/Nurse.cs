5f8a735144f92a94399f57483b1724a7015a6a8f
NBeorn <nmbeorn@gmail.com>
2017-10-12 13:54:10 -0400

    added nurse class

---
+++ Hospital/Nurse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    class Nurse : Non_Doctor
+    {
+        private int numberOfPatients;
+
+        public int NumberOfPatients
+        {
+            get { return this.numberOfPatients; }
+        }
+
+        public Nurse(string employeeName, int employeeNumber, string department, int numberOfPatients)
+        {
+            this.employeeName = employeeName;
+            this.employeeNumber = employeeNumber;
+            this.department = department;
+            this.numberOfPatients = numberOfPatients;
+        }
+    }
+}
