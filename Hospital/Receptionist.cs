d7a2b7db843449ad7943d0e8c1c9bbab1c7da1e2
NBeorn <nmbeorn@gmail.com>
2017-10-12 13:50:20 -0400

    added receptionist class

---
+++ Hospital/Receptionist.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    class Receptionist : Non_Doctor
+    {
+        private bool onThePhone;
+
+        public bool OnThePhone
+        {
+            get { return this.onThePhone; }
+        }
+
+        public Receptionist(string employeeName, int employeeNumber, string department, bool onThePhone)
+        {
+            this.employeeName = employeeName;
+            this.employeeNumber = employeeNumber;
+            this.department = department;
+            this.onThePhone = onThePhone;
+        }
+    }
+}
