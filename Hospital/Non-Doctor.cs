47d1b70a3d146ecf1380d929b35a0a6ed3cd4919
NBeorn <nmbeorn@gmail.com>
2017-10-12 13:46:24 -0400

    added non-doctor class

---
+++ Hospital/Non-Doctor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    abstract class Non_Doctor : Employee
+    {
+        protected string department;
+
+        public string Department
+        {
+            get { return this.department; }
+        }
+
+        public Non_Doctor()
+        {
+
+        }
+    }
+}
