f7edd40e2beabe13c948b532d5e17d2797700b0d
NBeorn <nmbeorn@gmail.com>
2017-10-12 14:04:43 -0400

    created employees

--- Hospital/Program.cs
+++ Hospital/Program.cs
@@ -10,6 +10,17 @@ namespace Hospital
     {
         static void Main(string[] args)
         {
+            //Employees
+            Doctor david = new Doctor("David Jones", 123, "heart");
+            Doctor james = new Doctor("James Smith", 342, "lungs");
+            Surgeon catherine = new Surgeon("Catherine Vincent", 725, "brain", true);
+            Surgeon jaime = new Surgeon("Jaime Rodriguez", 843, "spine", false);
+            Receptionist zachary = new Receptionist("Zachary Black", 186, "Office", true);
+            Receptionist emma = new Receptionist("Emma Thompson", 865, "Customer Service", false);
+            Nurse clara = new Nurse("Clara Kane", 527, "In-Patient", 4);
+            Nurse kyle = new Nurse("Kyle James", 142, "Out-Patient", 2);
+
+            Console.WriteLine("High St. Hospital Employees: \n");
         }
     }
 }
